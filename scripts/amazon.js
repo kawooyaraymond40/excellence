3d8ab4674bffc19e6878a3032228db3296f10883
banksray206 <banksray206@gmail.com>
2025-04-17 02:50:34 +0300

    changing the name to index

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,5 +8,24 @@ const products = [
     },
     price: 1090
 
+  },
+  {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    price: 2095
+
+  },{
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    price: 799
+
   }
 ];
