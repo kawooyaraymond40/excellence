c3d67c949937f0bb7beeacd091457b4df430fa7e
banksray206 <banksray206@gmail.com>
2025-04-17 23:19:24 +0300

    make acart button inttrative

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,8 +2,8 @@
 let productsHTML = '';
 
 products.forEach((product) =>{
-productsHTML += `
-<div class="product-container">
+productsHTML += 
+`<div class="product-container">
 <div class="product-image-container">
   <img class="product-image"
     src="${product.image}">
@@ -47,19 +47,43 @@ ${product.name}
   Added
 </div>
 
-<button class="add-to-cart-button button-primary">
+<button class="add-to-cart-button js-add-to-cart-button button-primary" data-product-id = "${product.id}"}>
   Add to Cart
 </button>
 </div>
  `
-
 }
-
 );
-console.log(productsHTML);
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId= button.dataset.productId;
 
+
+let matchingItem;
+    cart.forEach((item) =>{
+      if(productId === item.productId){
+       matchingItem  = item ;
+      }
+    });
+
+      if(matchingItem){
+        matchingItem.Quantity += 1;
+       } else {
+        cart.push(          
+            {
+              productId: productId,
+              Quantity: 1,
+            });
+      };
+console.log(cart);
+    
+
+    
+  });
+});
 
 
 
