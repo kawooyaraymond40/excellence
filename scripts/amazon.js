6d28f2fbbe181eec9e68275541e636ebf4d7a258
banksray206 <banksray206@gmail.com>
2025-04-17 14:08:15 +0300

    generating html with javascript

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -6,7 +6,7 @@ const products = [
       stars: 4.5,
       count: 87,
     },
-    price: 1090
+    priceCents: 1090
 
   },
   {
@@ -16,7 +16,7 @@ const products = [
       stars: 4,
       count: 127,
     },
-    price: 2095
+    priceCents: 2095
 
   },{
     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
@@ -25,7 +25,71 @@ const products = [
       stars: 4.5,
       count: 56,
     },
-    price: 799
+    priceCents: 799
 
   }
 ];
+  
+let productHTML = '';
+
+products.forEach((product) =>{
+productsHTML +=`
+<div class="product-container">
+<div class="product-image-container">
+  <img class="product-image"
+    src="${product.image}">
+</div>
+
+<div class="product-name limit-text-to-2-lines">
+${product.name}
+</div>
+
+<div class="product-rating-container">
+  <img class="product-rating-stars"
+    src="images/ratings/rating-${products.rating*10}.png">
+  <div class="product-rating-count link-primary">
+    ${product.rating.count} 
+  </div>
+</div>
+
+<div class="product-price">
+  $${(product.priceCents/100).fixed(2)}
+</div>
+
+<div class="product-quantity-container">
+  <select>
+    <option selected value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+  </select>
+</div>
+
+<div class="product-spacer"></div>
+
+<div class="added-to-cart">
+  <img src="images/icons/checkmark.png">
+  Added
+</div>
+
+<button class="add-to-cart-button button-primary">
+  Add to Cart
+</button>
+</div>
+ `
+
+}
+
+);
+
+document.querySelector('.products-grid').innerHTML = productHTML;
+
+
+
+
