10fa0965c51a6f4dcab6a7f7c8983dd4fd1a2e11
banksray206 <banksray206@gmail.com>
2025-04-20 19:39:33 +0300

    delete products from cart when click delete link

--- data/cart.js
+++ data/cart.js
@@ -28,4 +28,18 @@
      };
  
  }
+
+
+ export function removeFromCart(productId ){
+const newCart =[];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+
+  });
  
+cart = newCart;
+};
+
