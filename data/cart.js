bb94722b2bc33a94e7b17ba915f07a7bdba91c74
banksray206 <banksray206@gmail.com>
2025-04-22 00:50:41 +0300

    save data and genearting data for delivery option

--- data/cart.js
+++ data/cart.js
@@ -3,9 +3,11 @@
  if(!cart){
  cart = [ { productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   Quantity: 2,
+  deliveryOptionsId: '1'
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   Quantity: 1,  
+  deliveryOptionsId: '2'
 }
 
  ]};
@@ -29,6 +31,7 @@
            {
              productId: productId,
              Quantity: 1,
+             deliveryOptions: '1'
            });
      };
  saveToStorage();
