fabc9e6d4ebf1e9400595227791fd9d3d0391a96
banksray206 <banksray206@gmail.com>
2025-04-30 23:24:55 +0300

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 
 import {renderPaymentSummary} from "./checkout/paymentSumarry.js"
 
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 
 renderOrderSummary();
