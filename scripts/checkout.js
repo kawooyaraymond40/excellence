8bda8652925e676cbe0508059134cd2bf0c7ff13
kawooyaraymond40 <kawooyaraymond40@gmail.com>
2025-07-25 23:21:17 +0300

    Delete scripts directory

--- scripts/checkout.js
+++
@@ -1,9 +0,0 @@
-import { renderOrderSummary } from "./checkout/orderSummary.js";  
-
-import {renderPaymentSummary} from "./checkout/paymentSumarry.js"
-
-//import '../data/cart-class.js';
-
-
-renderOrderSummary();
-renderPaymentSummary(); 
